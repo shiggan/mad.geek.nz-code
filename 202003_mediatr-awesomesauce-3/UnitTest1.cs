65c41e286feb49a55c89191a81db9187f7a7c5fd
Steven Higgan <stevenh@mad.geek.nz>
2020-12-17 23:25:03 +1300

    wip

--- 202003_mediatr-awesomesauce-3/UnitTest1.cs
+++ 202003_mediatr-awesomesauce-3/UnitTest1.cs
@@ -4,7 +4,12 @@ using FluentAssertions;
 
 using MediatR;
 
+using System;
+
+using System.Reflection;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 
 using Xunit;
 
@@ -13,7 +18,7 @@ namespace _202002_mediatr_awesomesauce_3
     public class UnitTest1
     {
       [Fact]
-        public async Task shows_handler_via_autofac ( )
+        public async Task shows_handler_behavior_discovery ( )
         {
           var builder = new ContainerBuilder ( );
 
@@ -26,6 +31,13 @@ namespace _202002_mediatr_awesomesauce_3
 
           var logger = new Logger ( );
           builder.RegisterInstance ( logger ).As<ILogger> ( ).SingleInstance ( );
+
+          // discover all request handlers
+          // for each handler we discover
+          // - discover type arguments
+          // - 
+
+          var requestHandlers = DiscoverRequestHandlers ( );
 
           // simply register the handler
           builder.RegisterType<Handler1> ( ).AsImplementedInterfaces ( );
@@ -47,5 +59,34 @@ namespace _202002_mediatr_awesomesauce_3
             }
           );
         }
+
+    private IEnumerable<TypeInfo> DiscoverRequestHandlers ( )
+    {
+      var query = new [] { typeof(UnitTest1).Assembly }
+        .Where ( a => !a.IsDynamic )
+        .Distinct ( )
+        .SelectMany ( a => a.DefinedTypes )
+        .ToArray ( );
+
+      var registerTypes = new [ ] {
+        typeof(IRequestHandler<,>)
+      };
+
+      var mediatrHandlers = registerTypes.SelectMany ( openType =>
+        query.Where (
+          t => t.IsClass
+            && !t.IsAbstract
+            && ImplementsGenericInterface ( t.AsType ( ), openType )
+        ) )
+        .ToList (  );
+
+      return mediatrHandlers;
     }
+
+    private static bool ImplementsGenericInterface ( Type type, Type interfaceType )
+      => IsGenericType ( type, interfaceType ) || type.GetTypeInfo ( ).ImplementedInterfaces.Any ( @interface => IsGenericType ( @interface, interfaceType ) );
+
+    private static bool IsGenericType ( Type type, Type genericType )
+      => type.GetTypeInfo ( ).IsGenericType && type.GetGenericTypeDefinition ( ) == genericType;
+  }
 }
